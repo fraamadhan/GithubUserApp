b2ab827ffba442c7bf4c7b9d9e586a0ea741a19a
fraamadhan <fakhrifajarrr@gmail.com>
2023-10-03 23:29:51 +0700

    feat : switch theme using datastore done

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,12 +52,11 @@ android {
             }
         }
     }
-
 }
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.viewpager2:viewpager2:1.0.0")
@@ -70,9 +69,14 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
 
+    implementation("androidx.datastore:datastore-preferences:1.0.0")
     implementation("androidx.room:room-runtime:2.5.2")
     implementation("androidx.room:room-ktx:2.5.2")
     ksp("androidx.room:room-compiler:2.5.2")
+
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
 
     implementation("androidx.activity:activity-ktx:1.7.2")
     implementation("androidx.fragment:fragment-ktx:1.6.1")
--- app/src/main/java/com/example/githubapp/ui/activity/SplashScreenActivity.kt
+++ app/src/main/java/com/example/githubapp/ui/activity/SplashScreenActivity.kt
@@ -5,7 +5,13 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.lifecycle.ViewModelProvider
 import com.example.githubapp.R
+import com.example.githubapp.ui.factory.PreferencesViewModelFactory
+import com.example.githubapp.ui.viewmodel.SettingViewModel
+import com.example.githubapp.util.SettingPreferences
+import com.example.githubapp.util.dataStore
 
 class SplashScreenActivity : AppCompatActivity() {
 
@@ -18,5 +24,19 @@ class SplashScreenActivity : AppCompatActivity() {
             startActivity(moveToMain)
             finish()
         }, 2000)
+
+        val pref = SettingPreferences.getInstance(application.dataStore)
+        val settingViewModel = ViewModelProvider(this, PreferencesViewModelFactory(pref)).get(
+            SettingViewModel::class.java
+        )
+
+        settingViewModel.getThemeSettings().observe(this){isDarkModeActive->
+            if(isDarkModeActive){
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            }
+            else{
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+        }
     }
 }
